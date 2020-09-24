696bf24f519c2c59f8b665139359c15b0dfeb21d
GillianY1 <gilliangilliany@gmail.com>
2020-09-24 23:45:40 +0800

    communicate from parent component to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUtl : 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    { title: '2',
+    imageUtl : 'assets/tree.jpeg',
+    username: 'nature',
+    content: '2I saw this neat tree today'
+    },
+    {  title: '3',
+    imageUtl : 'assets/tree.jpeg',
+    username: 'nature',
+    content: '2I saw this neat tree today'
+    }
+  ]
+
 }
