5b1bac2ef353ef275d8ea97ea27357b0fc522d04
GillianY1 <gilliangilliany@gmail.com>
2020-09-25 21:37:12 +0800

    finish 3 cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,14 @@ export class AppComponent {
       content: 'I saw this neat tree today'
     },
     { title: '2',
-    imageUtl : 'assets/tree.jpeg',
-    username: 'nature',
-    content: '2I saw this neat tree today'
+    imageUtl : 'assets/biking.jpeg',
+    username: 'cat',
+    content: 'I saw a cat on the road'
     },
     {  title: '3',
-    imageUtl : 'assets/tree.jpeg',
-    username: 'nature',
-    content: '2I saw this neat tree today'
+    imageUtl : 'assets/mountain.jpeg',
+    username: 'dog',
+    content: 'let us go to the mountain'
     }
   ]
 
